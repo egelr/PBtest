98b633f1898ccd8fe47bc03ea3581cec00287262
egelr <egle.rupeikyt@gmail.com>
2024-01-04 19:39:46 +0200

    code?!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -1,12 +1,20 @@
 package org.firstinspires.ftc.teamcode;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 
 @TeleOp(name = "Encoder Test ")
 public class Arm extends LinearOpMode {
     private DcMotor armLiftMotor;
+    private DcMotor armSlideMotor;
     static final double MOTOR_TICK_COUNT = 8192;
 
     public void runOpMode() throws InterruptedException {
@@ -14,60 +22,107 @@ public class Arm extends LinearOpMode {
         int quarterTurn = 8192;
 
         Motor m_motor = new Motor(hardwareMap, "armLiftMotor", 8192, 60);
-
+        Motor m_motor_2 = new Motor(hardwareMap, "armSlideMotor", 1425.2, 117);
         telemetry.addData("Hardware: ", "Initialized");
         telemetry.update();
         m_motor.setRunMode(Motor.RunMode.PositionControl);
+        m_motor_2.setRunMode(Motor.RunMode.PositionControl);
         //m_motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
 
 // set and get the position coefficient
         m_motor.setPositionCoefficient(0.05);
         double kP = m_motor.getPositionCoefficient();
+        m_motor_2.setPositionCoefficient(0.05);
+        double kP_2 = m_motor_2.getPositionCoefficient();
 
-// set the target position
-        m_motor.encoder.reset();
-        m_motor.setInverted(false);
-        m_motor.setTargetPosition(-500);      // an integer representing
-        // desired tick count
 
-        m_motor.set(0);
+        //m_motor_2.setInverted(true);
+        int pos = m_motor.getCurrentPosition();
+        //m_motor.setTargetPosition(pos);
+        m_motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
 
+        m_motor_2.setInverted(true);
+        //int pos_2 = m_motor_2.getCurrentPosition();
+        m_motor_2.resetEncoder();
+        //m_motor_2.getInverted(false);
+        //int targetPos_2 = pos_2;
+        //m_motor_2.setTargetPosition(pos_2);
+        m_motor_2.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        m_motor_2.set(0);
 // set the tolerance
-        m_motor.setPositionTolerance(13.6);   // allowed maximum error
+        m_motor.setPositionTolerance(30);
+        m_motor_2.setPositionTolerance(5);// allowed maximum error
+        //GamepadEx myGamepad = new Gamepad(gamepad1);
+        double sp = 0.02;
+        telemetry.addData("Status m2", m_motor_2.getCurrentPosition());
+        telemetry.update();
 
         waitForStart();
 
-        // Set motor mode and reset encoder
-        //armLiftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //armLiftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    //    armLiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        //armLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //int newTarget = armLiftMotor.getTargetPosition() + 1000;
-//        int newTarget = 500;
-        //armLiftMotor.setTargetPosition(newTarget);
-        //armLiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //armLiftMotor.setPower(0.5);
-
-        // set the run mode
 
 // perform the control loop
-        while (!m_motor.atTargetPosition()) {
-            m_motor.set(0.1);
-            telemetry.addData("Status ", m_motor.getCurrentPosition());
-            telemetry.update();
+        while (opModeIsActive()) {
 
+            if (gamepad1.right_bumper) {
+                sp = 0.006;
+                m_motor.setTargetPosition(1540);
+            }
+            if (gamepad1.left_bumper) {
+                sp = 0.01;
+                m_motor.setTargetPosition(-1450);
+            }
+
+            while (!m_motor.atTargetPosition()) {
+                m_motor.set(sp);
+                telemetry.addData("Status ", m_motor.getCurrentPosition());
+                telemetry.update();
+
+            }
+
+
+
+            if (gamepad1.square) {
+          //      sp = 0.02;
+                m_motor_2.setTargetPosition(1000);
+
+                telemetry.addData("share", "lllll");
+                telemetry.addData("Status ", m_motor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            if (gamepad1.triangle) {
+            //    sp = 0.02;
+                m_motor_2.setTargetPosition(0);
+                telemetry.addData("options", "llll");
+                telemetry.addData("Status ", m_motor.getCurrentPosition());
+                telemetry.update();
+                //sp = 3;
+                //m_motor_2.setRunMode(Motor.RunMode.RawPower);
+
+            }
+
+
+            while (!m_motor_2.atTargetPosition()) {
+                m_motor_2.set(0.2);
+                telemetry.addData("Status", "Moving to target position");
+                telemetry.addData("Current Position", m_motor_2.getCurrentPosition());
+          //    telemetry.addData("Target Position", targetPos_2);
+                telemetry.update();
+
+            }
+
+
+            m_motor.stopMotor(); // stop the motor
+            m_motor_2.stopMotor(); // stop the motor
+        //m_motor_2.stopMotor();
+            sleep(10);
         }
-        m_motor.stopMotor(); // stop the motor
-
-        //while (opModeIsActive() && armLiftMotor.isBusy()) {
-        //    telemetry.addData("Status ", armLiftMotor.getCurrentPosition());
-         //   telemetry.update();
-       // }
-        //armLiftMotor.setPower(0);
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
 
     }
+
 }
+
+
+
+
 
